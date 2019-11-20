dae01d9a5545ee2aa95be42eec6afc02a7db069b
Massimo Tascone <tascone.massimo@gmail.com>
2019-11-20 10:40:28 +0100

    v-if and v-for

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,22 @@ new Vue({
     el: '#title',
     data: {
         title: 'Site vers Durga',
-        link: 'http://www.durgasoft.com'
+        link: 'http://www.durgasoft.com',
+        icon: true,
+        
+    },
+
+    methods: {
+        click: function() {
+            this.icon = false
+        }
+    }
+});
+
+new Vue({
+    el: '.array',
+    data: {
+        persons: ['Chin', 'Matis', 'Mass', 'Pierre', 'Jeason', 'Octav', 'Lal'],
+
     }
 })
